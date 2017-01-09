10e9295e772b1380ce17cf88b1bf2c3795ba082b
rawanth <rawan.k.th@gmail.com>
2017-01-09 20:10:08 +0300

    fix bugs in all Studyrooms and userprofile

--- app/src/main/java/com/example/nuhakhayat/quizzy/StudyRoom/AllStudyRoomsFragment.java
+++ app/src/main/java/com/example/nuhakhayat/quizzy/StudyRoom/AllStudyRoomsFragment.java
@@ -24,6 +24,7 @@ public class AllStudyRoomsFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		db=new Database(getActivity().getApplicationContext());
+
 		view = inflater.inflate(R.layout.fragment_all_study_rooms,container,false);
 		ListView listView = (ListView)view.findViewById(R.id.listViewall);
 
